aa0e7ca8728e132f3d4b19f2f26820d2842f5fec
alper-han <89567766+alper-han@users.noreply.github.com>
2026-01-29 11:20:07 +0300

    refactor(intel): update video driver configuration

--- modules/hardware/video/intel.nix
+++ modules/hardware/video/intel.nix
@@ -1,12 +1,7 @@
-{
-  pkgs,
-  lib,
-  config,
-  ...
-}:
+{ pkgs, lib, config, ... }:
 {
   nixpkgs.config.packageOverrides = pkgs: {
-    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+    intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
   };
 
   environment.sessionVariables = lib.optionalAttrs config.programs.hyprland.enable {
@@ -32,8 +27,8 @@
     enable = true;
     extraPackages = with pkgs; [
       intel-media-driver
-      vaapiIntel
-      vaapiVdpau
+      intel-vaapi-driver
+      libva-vdpau-driver
       libvdpau-va-gl
     ];
   };
