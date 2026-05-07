5528c237bf57975f2482dc53547befb15d5cb3e7
alper-han <89567766+alper-han@users.noreply.github.com>
2026-05-07 08:36:59 +0300

    refactor(nvidia): use production open driver

--- modules/hardware/video/nvidia.nix
+++ modules/hardware/video/nvidia.nix
@@ -1,11 +1,10 @@
 {
   lib,
-  pkgs,
   config,
   ...
 }:
 let
-  nvidiaDriverChannel = config.boot.kernelPackages.nvidiaPackages.beta; # stable, latest, beta, etc.
+  nvidiaDriverChannel = config.boot.kernelPackages.nvidiaPackages.production; # stable, latest, beta, etc.
 in
 {
   environment.sessionVariables = lib.optionalAttrs config.programs.hyprland.enable {
@@ -14,21 +13,16 @@ in
     # WLR_NO_HARDWARE_CURSORS = "1";
     LIBVA_DRIVER_NAME = "nvidia";
     __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-    __GL_MaxFramesAllowed = "1"; # Reduces input lag
-    __GL_SYNC_TO_VBLANK = "0"; # Disable VSync for lower input lag (works with allow_tearing)
+    # __GL_MaxFramesAllowed = "1"; # Reduces input lag
+    # __GL_SYNC_TO_VBLANK = "0"; # Disable VSync for lower input lag (works with allow_tearing)
   };
 
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = [ "nvidia" ]; # or "nvidiaLegacy470", etc.
-  boot.kernelParams = lib.optionals (lib.elem "nvidia" config.services.xserver.videoDrivers) [
-    "nvidia-drm.modeset=1"
-    "nvidia_drm.fbdev=1"
-    "nvidia.NVreg_RegistryDwords=RmEnableAggressiveVblank=1" # Experimental: reduce latency
-  ];
   hardware = {
     nvidia = {
-      open = false; #nvdec performance fix
-      # nvidiaPersistenced = true;
+      open = true; # nvdec performance fix
+      nvidiaPersistenced = true;
       nvidiaSettings = true;
       powerManagement.enable = true; # This can cause sleep/suspend to fail.
       modesetting.enable = true;
@@ -37,15 +31,15 @@ in
     graphics = {
       enable = true;
       enable32Bit = true;
-      extraPackages = with pkgs; [
-        nvidia-vaapi-driver
-        libva-vdpau-driver
-        libvdpau-va-gl
+      extraPackages = [
+        # pkgs.nvidia-vaapi-driver
+        # pkgs.libva-vdpau-driver
+        # pkgs.libvdpau-va-gl
       ];
     };
   };
   nixpkgs.config = {
     nvidia.acceptLicense = true;
-    cudaSupport = true;
+    # cudaSupport = true;
   };
 }
