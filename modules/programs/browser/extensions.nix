707f337f8329793f589c3af9aa9438b7da77b95b
alper-han <89567766+alper-han@users.noreply.github.com>
2026-06-04 07:35:39 +0300

    refactor(browser): simplify uBlock settings

--- modules/programs/browser/extensions.nix
+++ modules/programs/browser/extensions.nix
@@ -104,23 +104,14 @@
       # disabledFor = [];
     };
     "uBlock0@raymondhill.net" = {
-      advancedSettings = [
-        [
-          "userResourcesLocation"
-          "https://raw.githubusercontent.com/pixeltris/TwitchAdSolutions/master/video-swap-new/video-swap-new-ublock-origin.js"
-        ]
-      ];
       adminSettings = {
-        userFilters = lib.concatMapStrings (x: x + "\n") [
-          "twitch.tv##+js(twitch-videoad)"
-        ];
         userSettings = rec {
           uiTheme = "dark";
           uiAccentCustom = true;
           uiAccentCustom0 = "#CA9EE6";
           cloudStorageEnabled = lib.mkForce false; # Security liability?
           advancedUserEnabled = true;
-          userFiltersTrusted = true;
+          showIconBadge = false;
           importedLists = [
             "https://raw.githubusercontent.com/reek/anti-adblock-killer/master/anti-adblock-killer-filters.txt"
             "https://easylist-downloads.adblockplus.org/antiadblockfilters.txt"
@@ -133,7 +124,7 @@
             "https://raw.githubusercontent.com/iam-py-test/my_filters_001/main/antimalware.txt"
             "https://raw.githubusercontent.com/liamengland1/miscfilters/master/antipaywall.txt"
             "https://raw.githubusercontent.com/yokoffing/filterlists/main/annoyance_list.txt"
-            "https://raw.githubusercontent.com/yokoffing/filterlists/main/privacy_essentials.txt"
+            # "https://raw.githubusercontent.com/yokoffing/filterlists/main/privacy_essentials.txt"
           ];
           externalLists = lib.concatStringsSep "\n" importedLists;
           popupPanelSections = 31;
@@ -172,18 +163,6 @@
           "adguard-widgets"
           "ublock-annoyances"
           "TUR-0"
-          "https://raw.githubusercontent.com/reek/anti-adblock-killer/master/anti-adblock-killer-filters.txt"
-          "https://easylist-downloads.adblockplus.org/antiadblockfilters.txt"
-          "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Dandelion Sprout's Anti-Malware List.txt"
-          "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/LegitimateURLShortener.txt"
-          "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/BrowseWebsitesWithoutLoggingIn.txt"
-          "https://raw.githubusercontent.com/yokoffing/filterlists/main/privacy_essentials.txt"
-          "https://raw.githubusercontent.com/yokoffing/filterlists/main/annoyance_list.txt"
-          "https://raw.githubusercontent.com/liamengland1/miscfilters/master/antipaywall.txt"
-          "https://gitflic.ru/project/magnolia1234/bypass-paywalls-clean-filters/blob/raw?file=bpc-paywall-filter.txt"
-          "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/ClearURLs for uBo/clear_urls_uboified.txt"
-          "https://raw.githubusercontent.com/iam-py-test/my_filters_001/main/antimalware.txt"
-          "https://raw.githubusercontent.com/bogachenko/fuckfuckadblock/master/fuckfuckadblock.txt?_=rawlist"
           "user-filters"
         ];
       };
