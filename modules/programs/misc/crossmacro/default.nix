af04291e54ec06563e387d22809728e69ee34b54
alper-han <89567766+alper-han@users.noreply.github.com>
2026-09-23 14:24:20 +0300

    refactor(overlays): remove obsolete custom packages

--- modules/programs/misc/crossmacro/default.nix
+++ modules/programs/misc/crossmacro/default.nix
@@ -1,18 +1,11 @@
 {
   host,
-  inputs,
   ...
 }:
 let
-  inherit (import ../../../../hosts/${host}/variables.nix)
-    username
-    ;
+  inherit (import ../../../../hosts/${host}/variables.nix) username;
 in
 {
-  imports = [
-    inputs.crossmacro.nixosModules.default
-  ];
-
   services.crossmacro = {
     enable = true;
     users = [ username ];
--- overlays/default.nix
+++ overlays/default.nix
@@ -1,37 +1,9 @@
-{ host, inputs, ... }:
 {
-  # Overlay custom derivations into nixpkgs so you can use pkgs.<name>
-  additions =
-    final: _prev:
-    import ../pkgs {
-      pkgs = final;
-      inherit host;
-    };
-
-  # https://wiki.nixos.org/wiki/Overlays
-  modifications = final: prev: {
-    # virtualisation.libvirtd.qemu.package uses pkgs.qemu_kvm.
-    # Pin qemu_kvm to 10.2.2 because the anti-detection patch is version-specific.
-    qemu_kvm =
-      let
-        qemuPkgs = inputs.nixpkgs-qemu-10-2-2.legacyPackages.${final.stdenv.hostPlatform.system};
-      in
-      qemuPkgs.qemu_kvm.overrideAttrs (oldAttrs: {
-        patches = (oldAttrs.patches or [ ]) ++ [
-          (final.fetchpatch {
-            url = "https://raw.githubusercontent.com/zhaodice/qemu-anti-detection/2750c86d2d045243ba6617951487e41b25c05557/qemu-10.2.2.patch";
-            hash = "sha256-mMUtKzkHh8Q1lBu2Lrok6au521mUf4KOj8QJZRPOCOQ=";
-          })
-        ];
-      });
-
+  # Runs after nix-cachyos-kernel so local overrides take precedence.
+  modifications = _final: prev: {
     vesktop = prev.vesktop.override {
       withTTS = false;
       withSystemVencord = false;
-      withMiddleClickScroll = true;
-    };
-    equibop = prev.equibop.override {
-      withTTS = false;
       withMiddleClickScroll = true;
     };
     discord = prev.discord.override {
@@ -39,12 +11,5 @@
       withOpenASAR = false;
       enableAutoscroll = true;
     };
-
-    davinci-resolve-studio =
-      inputs.nixpkgs-davinci-20-2-3.legacyPackages.${final.stdenv.hostPlatform.system}.davinci-resolve-studio;
-
-    #    nvidia-vaapi-driver = prev.nvidia-vaapi-driver.overrideAttrs (oldAttrs: {
-    #      src = /home/zynix/Downloads/encode/nvidia-vaapi-driver;
-    #    });
   };
 }
