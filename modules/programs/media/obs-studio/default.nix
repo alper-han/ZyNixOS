f6424167f3b7dadf414dff8f64abcd2d85429279
alper-han <89567766+alper-han@users.noreply.github.com>
2026-09-23 14:24:21 +0300

    refactor(media): remove obsolete OBS and Spicetify integrations

--- modules/programs/media/obs-studio/default.nix
+++ modules/programs/media/obs-studio/default.nix
@@ -5,7 +5,6 @@
       programs.obs-studio = {
         enable = true;
         plugins = with pkgs.obs-studio-plugins; [
-          wlrobs
           obs-vaapi
           obs-vkcapture
           obs-pipewire-audio-capture
